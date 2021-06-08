e67ed94141e88f0604a60472e37d34d662388b84
chernobyl-worker <53166158+chernobyl-worker@users.noreply.github.com>
2021-06-08 21:58:48 +0530

    Update catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 
 // Require our controllers.
-var book_controller = require('../controllers/bookController'); 
-var author_controller = require('../controllers/authorController');
+var book_controller = require('../controllers/bookcontroller'); 
+var author_controller = require('../controllers/authorcontroller');
 var genre_controller = require('../controllers/genrecontroller');
-var book_instance_controller = require('../controllers/bookinstanceController');
+var book_instance_controller = require('../controllers/bookinstancecontroller');
 
 
 /// BOOK ROUTES ///
